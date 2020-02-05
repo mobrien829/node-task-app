ebccb4fc62c5a1c7a992b753d70bd8388cd9a6f6
Michael <mobrien@colgate.edu>
2020-02-05 15:39:45 -0500

    added files for playing with async/await

---
+++ playground/promise-chaining-2.js
@@ -0,0 +1,28 @@
+require("../src/db/mongoose");
+const Task = require("../src/models/task");
+
+// Task.findByIdAndDelete("5e3b14b63825282f08806cae")
+//   .then(task => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+const deleteTaskAndCount = async id => {
+  const task = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return count;
+};
+
+deleteTaskAndCount("5e3b27b100768f33effa8ecb")
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.log(error);
+  });
