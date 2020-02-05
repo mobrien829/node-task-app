3fd1e23277995c3e44f1be650c6466baca19c084
Michael <mobrien@colgate.edu>
2020-02-04 19:13:31 -0500

    continuing with mongoose lessons

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,23 +6,46 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useUnifiedTopology: true
 });
 
-const User = mongoose.model("User", {
-  name: {
+// const User = mongoose.model("User", {
+//   name: {
+//     type: String
+//   },
+//   age: {
+//     type: Number
+//   }
+// });
+
+// const me = new User({
+//   name: "Michael",
+//   age: 25
+// });
+
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+const Task = mongoose.model("Task", {
+  description: {
     type: String
   },
-  age: {
-    type: Number
+  completed: {
+    type: Boolean
   }
 });
 
-const me = new User({
-  name: "Michael",
-  age: 25
+const newTask = new Task({
+  description: "study pls",
+  completed: false
 });
 
-me.save()
+newTask
+  .save()
   .then(() => {
-    console.log(me);
+    console.log(newTask);
   })
   .catch(error => {
     console.log(error);
