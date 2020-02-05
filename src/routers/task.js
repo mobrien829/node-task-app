2bf49533c71aa6928dc95f8fb08e2aedac5762a4
Michael <mobrien@colgate.edu>
2020-02-05 17:01:21 -0500

    moved routes into their own directory

---
+++ src/routers/task.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const router = new express.Router();
+const Task = require("../models/task");
+
+router.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).send(tasks);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.get("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.status(200).send(task);
+  } catch (error) {
+    res.send(500).send();
+  }
+});
+
+router.post("/tasks", async (req, res) => {
+  const task = new Task(req.body);
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.patch("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+  const allowedUpdates = ["description", "completed"];
+  const updates = Object.keys(req.body);
+  const validUpdate = updates.every(update => allowedUpdates.includes(update));
+
+  if (!validUpdate) {
+    return res.status(400).send({ error: "invalid update" });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.delete("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findByIdAndDelete(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
