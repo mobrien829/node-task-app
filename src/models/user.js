427eb43f61e57fe20efd86064ff918245b6e4554
Michael <mobrien@colgate.edu>
2020-02-13 14:53:09 -0500

    added user-task one to many relationship

--- src/models/user.js
+++ src/models/user.js
@@ -66,6 +66,12 @@ userSchema.statics.findByCredentials = async (email, password) => {
   return user;
 };
 
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner"
+});
+
 userSchema.methods.generateAuthToken = async function() {
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, "thisisblah");
