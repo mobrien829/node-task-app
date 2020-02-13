23685260751e26554009298a494f0242ccf86c10
Michael <mobrien@colgate.edu>
2020-02-12 23:55:28 -0500

    added logout route

--- src/routers/user.js
+++ src/routers/user.js
@@ -71,6 +71,20 @@ router.post("/users/login", async (req, res) => {
     res.status(400).send();
   }
 });
+
+// logout user
+
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.status.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
 // delete user
 router.delete("/users/:id", async (req, res) => {
   const _id = req.params.id;
