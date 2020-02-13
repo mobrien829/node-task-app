132701ca8422a7688bd65bdc5c33acb42dc791e5
Michael <mobrien@colgate.edu>
2020-02-13 16:01:58 -0500

    added cascade delete for tasks

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Task = require("./task");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -90,6 +91,12 @@ userSchema.methods.toJSON = function() {
 
   return userObject;
 };
+// delete tasks when user is removed
+userSchema.pre("remove", async function(next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
+  next();
+});
 
 userSchema.pre("save", async function(next) {
   const user = this;
