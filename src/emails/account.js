a1ad666d57197c095e45be55356c703773cb9569
Michael <mobrien@colgate.edu>
2020-04-07 19:56:11 -0400

    created gitignore

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,1 +1,27 @@
-const sendgridAPIKey =
+const sendgridAPIKey = process.env.SENDGRID_API_KEY;
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(sendgridAPIKey);
+
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "mobrien@colgate.edu",
+    subject: "Thanks for joining",
+    text: `Welcome to the app, ${name}. Let us know how you feel about the app!`
+  });
+};
+
+const sendCancelEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "mobrien@colgate.edu",
+    subject: "Cancellation confirmed",
+    text: `Sorry to see you go ${name}. Please let us know if there was anything we could have done to keep you on.`
+  });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancelEmail
+};
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,6 +4,7 @@ const User = require("../models/user");
 const auth = require("../middleware/auth.js");
 const multer = require("multer");
 const sharp = require("sharp");
+const { sendWelcomeEmail, sendCancelEmail } = require("../emails/account");
 
 const upload = multer({
   limits: {
@@ -24,6 +25,7 @@ router.post("/users", async (req, res) => {
 
   try {
     await user.save();
+    sendWelcomeEmail(user.email, user.name);
     const token = await user.generateAuthToken();
     res.status(201).send({ user, token });
   } catch (error) {
@@ -141,6 +143,7 @@ router.post("/users/logoutAll", auth, async (req, res) => {
 router.delete("/users/me", auth, async (req, res) => {
   try {
     await req.user.remove();
+    sendCancelEmail(req.user.email, req.user.name);
     res.send(req.user);
   } catch (error) {
     res.status(500).send();
